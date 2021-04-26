37effe2c7a2d1a68ac0ed3d3e8aabd839cd5aa36
Phil Reynolds <phil.d.reynolds@gmail.com>
2021-04-26 13:28:01 -0500

    Through Lesson 22

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,32 +1,23 @@
 <template>
   <div class="post-list">
-
     <div v-for="post in posts"
          :key="post.id"
          class="post"
     >
-
       <div class="user-info">
         <a href="#" class="user-name">{{userById(post.userId).name}}</a>
-
         <a href="#">post
           <img class="avatar-large" :src="userById(post.userId).avatar" alt="">
         </a>
-
         <p class="desktop-only text-small">107 posts</p>
-
       </div>
-
       <div class="post-content">
         <div>
           <p>{{post.text}}</p>
         </div>
       </div>
-
       <div class="post-date text-faded">{{post.publishedAt}}</div>
-
     </div>
-
   </div>
 </template>
 
