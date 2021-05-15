7672fde1c96afe984b45b61851630089b790cbea
Phil Reynolds <phil.d.reynolds@gmail.com>
2021-05-15 01:29:07 -0500

    Lesson 36 - Extracting User Posts and Threads into the Store.

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,32 +3,7 @@
     <div class="flex-grid">
       <div class="col-3 push-top">
 
-        <div class="profile-card">
-
-          <p class="text-center">
-            <img :src="user.avatar" :alt="`${ user.name } profile picture`" class="avatar-xlarge">
-          </p>
-
-          <h1 class="title">{{ user.username }}</h1>
-
-          <p class="text-lead">{{ user.name }}</p>
-
-          <p class="text-justify">
-            {{ user.bio || 'No bio specified'}}
-          </p>
-
-          <span class="online">{{ user.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr>
-
-          <p v-if="user.website" class="text-large text-center"><i class="fa fa-globe"></i> <a :href="user.website">{{ user.website }}</a></p>
-
-        </div>
+        <UserProfileCard :user="user" />
 
         <p class="text-xsmall text-faded text-center">Member since june 2003, last visited 4 hours ago</p>
 
@@ -49,7 +24,7 @@
         </div>
 
         <hr>
-        <PostList :posts="userPosts"></PostList>
+        <PostList :posts="user.posts"></PostList>
       </div>
     </div>
   </div>
@@ -57,28 +32,18 @@
 
 <script>
 import PostList from '@/components/PostList'
+import UserProfileCard from '@/components/UserProfileCard'
 
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Profile',
   components: {
-    PostList
+    PostList,
+    UserProfileCard
   },
   computed: {
-    ...mapGetters({ user: 'authUser' }),
-    userPosts () {
-      return this.$store.state.posts.filter(p => p.userId === this.user.id)
-    },
-    userPostsCount () {
-      return this.userPosts.length
-    },
-    userThreads () {
-      return this.$store.state.threads.filter(t => t.userId === this.user.id)
-    },
-    userThreadsCount () {
-      return this.userThreads.length
-    }
+    ...mapGetters({ user: 'authUser' })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,25 @@ export default createStore({
     authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
   },
   getters: {
-    authUser: state => state.users.find(user => user.id === state.authId)
+    authUser: state => {
+      const user = state.users.find(user => user.id === state.authId)
+      if (!user) return null
+      return {
+        ...user,
+        get posts () {
+          return state.posts.filter(p => p.userId === user.id)
+        },
+        get postsCount () {
+          return this.posts.length
+        },
+        get threads () {
+          return state.threads.filter(t => t.userId === user.id)
+        },
+        get threadsCount () {
+          return this.threads.length
+        }
+      }
+    }
   },
   actions: {
     createPost (context, post) {
