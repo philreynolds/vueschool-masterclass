9d45420cdcaa3296855bcfbc20135066c5e3086c
Phil Reynolds <phil.d.reynolds@gmail.com>
2021-06-13 01:16:25 -0500

    Lesson 53 - Install Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyDG-qmGev9nYjxQ7e1ifzmQ67FLfsTFP1c',
+  authDomain: 'vue-school-forum-60ee2.firebaseapp.com',
+  projectId: 'vue-school-forum-60ee2',
+  storageBucket: 'vue-school-forum-60ee2.appspot.com',
+  messagingSenderId: '154042220917',
+  appId: '1:154042220917:web:119993414382c8a172133c'
+}
