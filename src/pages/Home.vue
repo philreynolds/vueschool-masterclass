81dad1cfbcbb3ebbe658d2babc057a52cfc4fb06
Phil Reynolds <phil.d.reynolds@gmail.com>
2021-07-05 09:45:59 -0500

    Lesson 60 - Fetching Data in beforeCreate on the Homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -18,6 +18,12 @@ export default {
     categories () {
       return this.$store.state.categories
     }
+  },
+  async beforeCreate () {
+    const categories = await this.$store.dispatch('fetchAllCategories')
+    const forumIds = categories.map(category => category.forums).flat()
+    this.$store.dispatch('fetchForums', { ids: forumIds })
+    console.log('Home.vue > beforeCreate()', this.categories)
   }
 }
 
