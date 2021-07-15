5d37bc90299e86c40c58ccc94c768e6330c86373
philreynolds <phil.d.reynolds@gmail.com>
2021-07-15 14:50:40 -0500

    Lesson 86 - Integrating Registration with Firebase Authentication

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -56,7 +56,7 @@ export default {
   },
   methods: {
     async register () {
-      await this.$store.dispatch('createUser', this.form)
+      this.$store.dispatch('registerUserWithEmailAndPassword', this.form)
       this.$router.push('/')
     }
   },
